7acb3cd7aca319c67b064851ad7f6d26e7b3e261
PedroInst <2904647@alu.murciaeduca.es>
2025-02-03 18:50:02 +0100

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -1,7 +1,18 @@
+import java.time.LocalTime;
+
 public class Principal {
     
     public static void main(String[] args){
         
         System.out.println("Hola Mundo");
+        System.out.println();
+
+        hora();
+    }
+
+    public static void hora() {
+
+        LocalTime horaActual = LocalTime.now();
+        System.out.println("Hora actual: " + horaActual);
     }
 }
