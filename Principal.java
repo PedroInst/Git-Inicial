f641aef7ac29dfe3887f1048554ab6fc8cecb085
PedroInst <2904647@alu.murciaeduca.es>
2025-02-03 18:43:36 +0100

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -10,7 +10,7 @@ public class Principal {
         hora();
     }
 
-    public static hora() {
+    public static void hora() {
 
         LocalTime horaActual = LocalTime.now();
         System.out.println("Hora actual: " + horaActual);
